a56f47ce882d4a786742ef1f5a938a6472cb40e5
zhaozhig <1067557360@qq.com>
2019-06-17 21:46:41 +0800

    getway

---
+++ getway/src/main/java/com/example/getway/security/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.example.getway.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * 安全配置相关
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+/**
+ *
+ *注意： @EnableGlobalMethodSecurity 可以配置多个参数:
+ *prePostEnabled :决定Spring Security的前注解是否可用 [@PreAuthorize,@PostAuthorize,..] 此处表明可用
+ *secureEnabled : 决定是否Spring Security的保障注解 [@Secured] 是否可用
+ *jsr250Enabled ：决定 JSR-250 annotations 注解[@RolesAllowed..] 是否可用.
+ */
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    //用户信息业务类
+   /* @Bean
+    public UserDetailsService userDetailsService() {
+        return new DomainUserDetailsService();
+    }*/
+    //密码加密器
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    //验证用户信息与密码
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder());
+    }
+   /* @Bean
+    public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
+        return new SecurityEvaluationContextExtension();
+    }*/
+    //不定义没有password grant_type
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
+
+
