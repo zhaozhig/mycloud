15b8cc332e3a9597dc6db50d919859a30be0b78a
zhaozg <1067557360@qq.com>
2019-06-18 17:37:05 +0800

    add code

---
+++ getway/src/main/java/com/example/getway/filter/CustomGlobalFileter.java
@@ -0,0 +1,45 @@
+package com.example.getway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author zhaozg
+ * @version 1.0
+ * @date 2019/6/18
+ */
+@Slf4j
+@Component
+public class CustomGlobalFileter implements GlobalFilter,Ordered {
+
+
+    private static final String COUNT_Start_TIME = "countStartTime";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        exchange.getAttributes().put(COUNT_Start_TIME, System.currentTimeMillis());
+        return chain.filter(exchange).then(
+                Mono.fromRunnable(() -> {
+                    Long startTime = exchange.getAttribute(COUNT_Start_TIME);
+                    Long endTime=(System.currentTimeMillis() - startTime);
+                    if (startTime != null) {
+                        log.info(exchange.getRequest().getURI().getRawPath() + ": " + endTime + "ms");
+                    }
+                })
+        );
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+
+}
